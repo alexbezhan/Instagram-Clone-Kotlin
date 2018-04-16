b72f1e17b1e39d4cec0e9f5d13c06cdf9b5114d2
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-04-16 10:37:09 +0300

    Bottom Toolbar [Ep.1]

---
+++ app/src/main/java/com/alexbezhan/instagram/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.alexbezhan.instagram
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
