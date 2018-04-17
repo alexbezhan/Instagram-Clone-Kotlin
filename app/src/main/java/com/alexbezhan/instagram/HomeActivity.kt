8c1010121d732e5c55ed6041d527ac4dea17148f
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-04-17 12:49:16 +0300

    Bottom navigation view.

--- app/src/main/java/com/alexbezhan/instagram/HomeActivity.kt
+++ app/src/main/java/com/alexbezhan/instagram/HomeActivity.kt
@@ -2,11 +2,20 @@ package com.alexbezhan.instagram
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        bottom_navigation_view.setIconSize(29f, 29f)
+        bottom_navigation_view.setTextVisibility(false)
+        bottom_navigation_view.enableItemShiftingMode(false)
+        bottom_navigation_view.enableShiftingMode(false)
+        bottom_navigation_view.enableAnimation(false)
+        for(i in 0 until bottom_navigation_view.menu.size()) {
+            bottom_navigation_view.setIconTintList(i, null)
+        }
     }
 }
