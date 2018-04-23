aeb76d05aa1e4928ecd78448ce861d6e86198095
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-04-23 17:34:31 +0300

    [Ep.8] - Sign in with Firebase.

--- app/src/main/java/com/alexbezhan/instagram/HomeActivity.kt
+++ app/src/main/java/com/alexbezhan/instagram/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.alexbezhan.instagram
 
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
@@ -11,5 +12,15 @@ class HomeActivity : BaseActivity(0) {
         setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
         setupBottomNavigation()
+
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("alex@alexbezhan.com", "password")
+                .addOnCompleteListener{
+                    if (it.isSuccessful) {
+                        Log.d(TAG, "signIn: success")
+                    } else {
+                        Log.e(TAG, "signIn: failure", it.exception)
+                    }
+                }
     }
 }
