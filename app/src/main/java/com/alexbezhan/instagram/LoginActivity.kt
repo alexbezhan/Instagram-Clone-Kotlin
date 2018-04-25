472bae71d3a7cf268d06c4019ebcc2c767e39060
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-04-25 17:13:44 +0300

    [Ep.10] - Login screen advanced UI.

--- app/src/main/java/com/alexbezhan/instagram/LoginActivity.kt
+++ app/src/main/java/com/alexbezhan/instagram/LoginActivity.kt
@@ -2,14 +2,46 @@ package com.alexbezhan.instagram
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher {
     private val TAG = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.d(TAG, "onCreate")
+
+        KeyboardVisibilityEvent.setEventListener(this, this)
+        login_btn.isEnabled = false
+        email_input.addTextChangedListener(this)
+        password_input.addTextChangedListener(this)
     }
+
+    override fun onVisibilityChanged(isKeyboardOpen: Boolean) {
+        if (isKeyboardOpen) {
+            scroll_view.scrollTo(0, scroll_view.bottom)
+            create_account_text.visibility = View.GONE
+        } else {
+            scroll_view.scrollTo(0, scroll_view.top)
+            create_account_text.visibility = View.VISIBLE
+        }
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        login_btn.isEnabled =
+                email_input.text.toString().isNotEmpty() &&
+                password_input.text.toString().isNotEmpty()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
 }
