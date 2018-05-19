764e4e0dd1b5123bb1c480edae1e78c516e2e8c2
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-05-19 15:58:45 +0300

    [Ep.24] - Fill in feed data.

--- app/src/main/java/com/alexbezhan/instagram/activities/HomeActivity.kt
+++ app/src/main/java/com/alexbezhan/instagram/activities/HomeActivity.kt
@@ -1,14 +1,24 @@
 package com.alexbezhan.instagram.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import com.alexbezhan.instagram.R
 import com.alexbezhan.instagram.utils.FirebaseHelper
 import com.alexbezhan.instagram.utils.GlideApp
@@ -64,13 +74,43 @@ class FeedAdapter(private val posts: List<FeedPost>)
         return ViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.view.post_image.loadImage(posts[position].image)
+        val post = posts[position]
+        with(holder) {
+            view.user_photo_image.loadImage(post.photo)
+            view.username_text.text = post.username
+            view.post_image.loadImage(post.image)
+            if (post.likesCount == 0) {
+                view.likes_text.visibility = View.GONE
+            } else {
+                view.likes_text.visibility = View.VISIBLE
+                view.likes_text.text = "${post.likesCount} likes"
+            }
+            view.caption_text.setCaptionText(post.username, post.caption)
+        }
+    }
+
+    private fun TextView.setCaptionText(username: String, caption: String) {
+        val usernameSpannable = SpannableString(username)
+        usernameSpannable.setSpan(StyleSpan(Typeface.BOLD), 0, usernameSpannable.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        usernameSpannable.setSpan(object: ClickableSpan() {
+            override fun onClick(widget: View) {
+                widget.context.showToast("Username is clicked")
+            }
+            override fun updateDrawState(ds: TextPaint?) { }
+        }, 0, usernameSpannable.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        text = SpannableStringBuilder().append(usernameSpannable).append(" ")
+                .append(caption)
+        movementMethod = LinkMovementMethod.getInstance()
     }
 
     override fun getItemCount() = posts.size
 
-    private fun ImageView.loadImage(image: String) {
+    private fun ImageView.loadImage(image: String?) {
         GlideApp.with(this).load(image).centerCrop().into(this)
     }
 }
